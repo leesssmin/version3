0280f1af99aca7c56644adf4b6a0b177c2670d3e
sm <tmxjel77777@daum.net>
2025-05-19 11:24:52 +0900

    bookDAO 클래스 구현완료

--- library_system/src/util/DatabaseUtil.java
+++ library_system/src/util/DatabaseUtil.java
@@ -2,6 +2,9 @@ package util;
 
 
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -10,9 +13,12 @@ import java.sql.SQLException;
  * @author 이승민
  * 데이터 베이스 연결을 관리하는 유틸리티 클래스
  */
+@Getter
+@Setter
+
 public class DatabaseUtil {
 
-    private static final String DB_URL = "jdbc:mysql://localhost:3306/mydatabase?serverTimezone=Asia/Seoul";
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/library?serverTimezone=Asia/Seoul";
     private static final String DB_USER = "root";
     private static final String DB_PASSWORD = "asd1234";
 
